9417f6eb763fc7c5a5e6f3e1b86c4613d2889f92
Uglypr1nces <shpatavdiu4@gmail.com>
2024-03-23 13:03:48 +0100

    added a round tracker

--- game/game.py
+++ game/game.py
@@ -2,16 +2,22 @@ import random
 from round import Round
 
 class Game:
-    choices = ["rock","paper","scissors"]
+    choices = ["rock", "paper", "scissors"]
+
+    def __init__(self):
+        self.rounds_played = 0
 
     def play(self):
         while True:
-            player_choice = input("what do you want to pick? (Rock, Paper or Scissors): ")
-            if player_choice.lower() in self.choices:
+            player_choice = input("What do you want to pick? (Rock, Paper, or Scissors): ").lower()
+            if player_choice in self.choices:
                 computer_choice = random.choice(self.choices)
-                round = Round(player_choice, computer_choice)
-                round.evaluate()
+                game_round = Round(player_choice, computer_choice)
+                self.rounds_played += 1
+                game_round.evaluate()
+                print(f"Rounds played: {self.rounds_played}")
             else:
                 print("Please pick one of the options listed.")
+
 game = Game()
 game.play()
